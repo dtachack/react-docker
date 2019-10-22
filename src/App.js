55e9b6aab34dee2ad892296345d7afbb86bc3752
David Niño <davidn@digitalware.com.co>
2019-10-22 08:50:43 -0500

    Creación del proyecto

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Arquitectura Empresarial de Software</p>
+        <p>
+          <b>&#8226; David Niño</b>
+          <br></br>
+          <b>&#8226; Miguel Sanchez</b>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
